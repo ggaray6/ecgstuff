c636bef7c369b8c7f497e5736754a4805f45ef3d
ggaray6 <124087949+ggaray6@users.noreply.github.com>
2024-02-29 22:37:42 -0600

    Update arduinocodereadfromoctave.ino

--- arduinocodereadfromoctave.ino
+++ arduinocodereadfromoctave.ino
@@ -1,2 +1,15 @@
+void setup() {
+  Serial.begin(9600); // Set the baud rate to match Octave code
+}
 
+void loop() {
+  // Check if data is available to read from serial
+  if (Serial.available() > 0) {
+    // Read the incoming byte
+    int data = Serial.parseInt(); // Assuming data is sent as integer
+    
+    // Plot data on Serial Plotter
+    Serial.println(data); // Print data to Serial Plotter
+  }
+}
 
